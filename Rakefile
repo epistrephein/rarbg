4d64ab1696bd480e94bc977af308919211a31724
Tommaso Barbato <epistrephein@gmail.com>
2019-02-14 07:28:55 +0100

    Add missing comments and uniform style

--- Rakefile
+++ Rakefile
@@ -9,4 +9,5 @@ RSpec::Core::RakeTask.new(:spec)
 RuboCop::RakeTask.new(:rubocop)
 YARD::Rake::YardocTask.new(:yard)
 
+# Run RuboCop and RSpec as default task.
 task default: %i[rubocop spec]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,24 +11,25 @@ require 'rarbg'
 require_relative 'stubs'
 
 RSpec.configure do |config|
+  # Attach WebMock and its stubbed requests.
   WebMock.disable_net_connect!(allow_localhost: true)
   config.include(Stubs)
 
-  # Enable flags like --only-failures and --next-failure
+  # Enable flags like --only-failures and --next-failure.
   config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  # Disable RSpec exposing methods globally on `Module` and `main`.
   config.disable_monkey_patching!
 
-  # Enable temporarily focused examples and groups
+  # Enable temporarily focused examples and groups.
   config.filter_run_when_matching :focus
 
-  # Use expect syntax
+  # Use expect syntax.
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
 
-  # Stub RATE_LIMIT to speed up tests
+  # Stub RATE_LIMIT to speed up tests.
   config.before(:example) do
     stub_const('RARBG::API::RATE_LIMIT', 0.1)
   end
